fe12ece6cfa55c7f4fd825214e9e93a3b7aff3db
Kelly Gitau <gitaukelly72@gmail.com>
2024-07-06 03:31:59 +0300

    Changed age from 25 to 23

--- src/Main.kt
+++ src/Main.kt
@@ -5,7 +5,7 @@ fun main() {
 
     userName = "Kevin"
 
-    val age: Int = 25
+    val age: Int = 23
     //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
     // to see how IntelliJ IDEA suggests fixing it.
     println("Hello, $userName!, your age is, $age")
